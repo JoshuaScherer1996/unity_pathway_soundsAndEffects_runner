c789663f9d359480db5b0a90e888e92985ec145d
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-08-15 10:36:59 +0200

    fixed both infinite obstacle spawn and obstacle despawn bugs

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -4,6 +4,7 @@ public class MoveLeft : MonoBehaviour
 {
     private const float Speed = 30.0f;
     private PlayerController _playerControllerScript;
+    private const float LeftBounds = -15.0f;
     
     // Start is called before the first frame update.
     private void Start()
@@ -17,6 +18,11 @@ public class MoveLeft : MonoBehaviour
         if (_playerControllerScript.gameOver == false)
         {
             transform.Translate(Vector3.left * (Time.deltaTime * Speed));
+        }
+
+        if (transform.position.x < LeftBounds && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,24 +3,28 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private readonly Vector3 _spawnPos = new (25, 0, 0);
+    private readonly Vector3 _spawnPos = new(25, 0, 0);
     private const float StartDelay = 2.0f;
     private const float RepeatDelay = 2.0f;
-    
+    private PlayerController _playerControllerScript;
+
     // Start is called before the first frame update
     private void Start()
     {
+        _playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating(nameof(SpawnObstacle), StartDelay, RepeatDelay);
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
     }
 
     private void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, _spawnPos, obstaclePrefab.transform.rotation);
+        if (_playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, _spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
