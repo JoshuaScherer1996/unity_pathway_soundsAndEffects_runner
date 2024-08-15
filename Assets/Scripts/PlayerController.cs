8184f2a0e3ba230f5c88de0eabe86c599b26e11b
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-08-15 11:27:40 +0200

    included a jumoing animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ public class PlayerController : MonoBehaviour
 {
     // Declaring and initializing the variables and constants.
     private Rigidbody _playerRb;
-    private const float JumpForce = 10.0f;
+    private Animator _playerAnim;
+    private const float JumpForce = 700.0f;
     private const float GravityModifier = 2.0f;
     private bool _isOnGround = true;
     public bool gameOver;
+    private static readonly int JumpTrig = Animator.StringToHash("Jump_trig"); // More efficient than using the name.
 
 
     // Start is called before the first frame update.
@@ -18,21 +20,27 @@ public class PlayerController : MonoBehaviour
 
         // Applying the GravityModifier.
         Physics.gravity *= GravityModifier;
+        
+        // Saves the Animator of the player.
+        _playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame.
     private void Update()
     {
-        // Makes the player jump if space bar is pressed.
+        // Makes the player jump and handles the jump animation logic.
         if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)
         {
             _playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
             _isOnGround = false;
+            _playerAnim.SetTrigger(JumpTrig);
         }
     }
 
+    // Invoked when the player collides with another object.
     private void OnCollisionEnter(Collision collision)
     {
+        // Executes the collision logic based on the tag of the collided object. 
         if (collision.gameObject.CompareTag("Ground"))
         {
             _isOnGround = true;
